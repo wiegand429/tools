64148fd665d6cd4b48a626b84797c73623b54c2e
wiegand <wiegand1992@163.com>
2017-12-13 11:02:57 +0800

    image display

--- UI.py
+++ UI.py
@@ -2,6 +2,7 @@ from ui_form import Ui_MainWindow
 from PyQt4 import QtGui
 from PyQt4 import QtCore
 import os
+import cv2
 
 class UI(QtGui.QMainWindow):
     def __init__(self,parent = None):
@@ -26,11 +27,28 @@ class UI(QtGui.QMainWindow):
         if self.ui_obj.image_path_lineEdit.text() != "":
             self.image_path = self.ui_obj.image_path_lineEdit.text()
             if os.path.exists(self.image_path):
-                pass
+                # print "cv2 imread path :"+str(self.image_path)
+                # image = cv2.imread(str(self.image_path))
+                # #print image.shape[1],image.shape[0]
+                # #print self.ui_obj.picture_display.width(),self.ui_obj.picture_display.height()
+                # qimage = QtGui.QImage(image, image.shape[1], image.shape[0], QtGui.QImage.Format_RGB888)
+                # qimage_resize = qimage.scaled(self.ui_obj.picture_display.width(), self.ui_obj.picture_display.height(), 0, 0)
+                # self.ui_obj.picture_display.setPixmap(QtGui.QPixmap.fromImage(qimage_resize))
+                # self.ui_obj.picture_display.show()
+
+                pixmap = QtGui.QPixmap()
+                pixmap.load(str(self.image_path))
+                newpixmap = pixmap.scaled(self.ui_obj.picture_display.width(), self.ui_obj.picture_display.height(), 0, 0)
+                self.ui_obj.picture_display.setPixmap(newpixmap)
+                self.ui_obj.picture_display.setAlignment(QtCore.Qt.AlignCenter)
+                self.ui_obj.picture_display.show()
+
+
+
             else:
                 print "path is not exists!!\n"
         else:
-            print "请输入图片路径\n"
+            print "pls fill imagepath\n"
 
 
         pass
